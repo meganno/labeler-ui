224bd2dc17a6b418bba84aeab4e2e1eb5e1ebe5b
meganno <99463168+meganno@users.noreply.github.com>
2022-12-06 23:35:23 -0800

    loading js from web

--- labeler_ui/__init__.py
+++ labeler_ui/__init__.py
@@ -13,6 +13,10 @@ _VERSION_PATH = Path(__file__).parent / 'version'
 VERSION = Path(_VERSION_PATH).read_text().strip()
 print("labeler-ui: " + VERSION)
 MODULE_NAME = f"labeler_ui@{VERSION}"
+
+_WEB_MODULE = idom.web.module_from_url(
+    "https://unpkg.com/labeler-ui@0.0.10/bundle.0.2.0.js")
+
 BUNDLE_PATH = Path(__file__).parent / BUNDLE_JS
 FALLBACK = "Failed to display labeler_ui widget."
 from .widgets.Annotation import Annotation
